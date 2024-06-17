aef92cfb221e631f71a2830a1f0b7dd090d20720
Allan Mwangi <allanmwangi800@gmail.com>
2024-06-17 23:17:25 +0300

    routed News component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
 
 export const routes: Routes = [
     { path: 'home',
         component: HomeComponent },
+    {
+        path: 'news',
+        component: NewsComponent
+    }
 ];
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css'
 })
