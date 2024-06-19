3ac57aa8747a4f2671d8a81dfddcbe5f62137335
Allan Mwangi <allanmwangi800@gmail.com>
2024-06-19 21:52:10 +0300

    Modified Components

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { PhotoComponent } from '../photo/photo.component';
+import { HomeComponent } from '../home/home.component';
+import { ContactComponent } from '../contact/contact.component';
+import { NewsComponent } from '../news/news.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  
 
 }
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, NavbarComponent],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css'
 })
