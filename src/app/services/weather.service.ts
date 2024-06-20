54a007cc9bc5796674442f67f35c24f022f49c9a
Allan Mwangi <allanmwangi800@gmail.com>
2024-06-20 09:02:32 +0300

    service update

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
-}
+  constructor(private httpClient: HttpClient) { 
+
+  }
+  get() : Observable<any>{
+    return this.httpClient.get<any>("http://api.openweathermap.org/data/2.5/weather");
+  }
+};
